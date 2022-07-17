c755ad336e963fc65ce93ac9e8ed089956e9bc34
Turza <cse.turza@gmail.com>
2022-07-17 12:18:59 +0600

    Add project files.

---
+++ csDemoArray/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csDemoArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new[] { 3, 7, 9, 2, 14, 6 };
+
+            //Length of array
+            Console.WriteLine("Length of Array : " + numbers.Length);
+
+        }
+    }
+}
